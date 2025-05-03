49a3a7a963a9b0a59aa9f483dfbb5191df31c102
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-05-03 22:01:33 +0600

    Weather_App_Using_javaScript

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const SearchBox = document.querySelector(".search input");
 const SearchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather_icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -17,12 +18,28 @@ async function checkWeather(city) {
 
     const data = await response.json();
 
-    console.log(data);
+    
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    } else {
+        weatherIcon.src = "images/default.png"; // Fallback icon
+    }
+
+    document.querySelector(".weather").style.display = "block";
 }
 
 SearchBtn.addEventListener("click", () => {
